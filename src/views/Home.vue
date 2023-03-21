fa647caadfcabb3a56e2db806f7ae0eaa2977e54
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-21 10:23:48 +0900

    ㅁㄴㅇㄹ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <VueWriter :array="['난 오늘 널 안내해줄']" :iterations="1" :start="900" :type-speed="150"/>
     <VueWriter :array="['★본듀꼰듀★']" :iterations="1" :start="2800" :type-speed="300" style="margin-bottom: 30px"/>
     <VueWriter :array="['내가 친절한 편이긴하지만']" :iterations="1" :start="5000" :type-speed="150"/>
-    <VueWriter :array="['그냥 안내해주면 재미없겠지?']" :iterations="1" :start="7000" :type-speed="150"/>
+    <VueWriter :array="['그냥 안내해주면 재미없겠지?']" :iterations="1" :start="7500" :type-speed="150"/>
     <VueWriter :array="['널 한 번 테스트해볼거야']" :iterations="1" :start="10000" :type-speed="150"/>
     <VueWriter :array="['마음의 준비가 됐으면']" :iterations="1" :start="12500" :type-speed="150" style="margin-bottom: 30px"/>
     <VueWriter :array="['가보자고~']" :iterations="1" :start="15000" :type-speed="150"/>
